def314a916387dfe84ab2aa6487990e03b486519
Zaheen Waseet <zwaseet@gmail.com>
2025-12-29 17:41:51 +0600

    Quartz sync: Dec 29, 2025, 5:41 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -14,7 +14,7 @@ const config: QuartzConfig = {
     enablePopovers: true,
     analytics: null,
     locale: "en-US",
-    baseUrl: "zaheen4.github.io",
+    baseUrl: "zaheen4.github.io/notes",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
